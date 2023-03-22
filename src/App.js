08efbc4d8d127dba7d19d6a58711fbf87bc374ab
Skinny_Vin <simonvicente38@gmail.com>
2023-03-23 01:18:20 +0800

    homepage update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,26 @@
 import Navigationbar from "./Components/NavigationBar";
-import { BrowserRouter, Routes, Route, Navigate } from  "react-router-dom";
-import AboutUs from './Pages/AboutUs';
-import Dashboard from './Pages/Dashboard';
-import Orders from './Pages/Orders';
-import Shop from './Pages/Shop';
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import AboutUs from "./Pages/AboutUs";
+import Dashboard from "./Pages/Dashboard";
+import Orders from "./Pages/Orders";
+import Shop from "./Pages/Shop";
 import Home from "./Pages/Home";
-
+import Body from "./Pages/Body";
 
 function App() {
   return (
-   <BrowserRouter>
-    <div className="App">
-
-      <Navigationbar />
-
-      <Routes>
-          <Route path = '/' element={<Home />}></Route>
-          <Route path = '/AboutUs' element={<AboutUs />}></Route>
-          <Route path = '/Dashboard' element={<Dashboard />}></Route>
-          <Route path = '/Orders' element={<Orders />}></Route>
-          <Route path = '/Shop' element={<Shop />}></Route>
-      </Routes>
-    
-    </div>
-   </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Navigationbar />
+        <Routes>
+          <Route path="/" element={<Body />}></Route>
+          <Route path="/AboutUs" element={<AboutUs />}></Route>
+          <Route path="/Dashboard" element={<Dashboard />}></Route>
+          <Route path="/Orders" element={<Orders />}></Route>
+          <Route path="/Shop" element={<Shop />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
