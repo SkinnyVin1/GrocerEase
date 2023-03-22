4a75e7e9b260b2e11212ac369729e888d0986a41
Skinny_Vin <118040316+SkinnyVin1@users.noreply.github.com>
2023-03-22 11:32:36 +0800

    homepage updates

--- src/Components/CardSmall.js
+++ src/Components/CardSmall.js
@@ -1,41 +1,45 @@
 import "../Components/CardSmall.css";
 import Image1 from "../Images/cardbg1.jpg";
+import Icecream from '../Images/icecream.jpg'
+import Milk from '../Images/milk (2).jpg';
+import Meat from '../Images/meat.jpg'
+import Veggies from '../Images/veggie.jpg'
 
 const CardSmall = (props) => {
   return (
     <div className="cardSmallWrapper">
       <div className="cardSmallBody">
-        <img src={Image1} alt="" />
+        <img src={Milk} alt="" />
         <div className="cardSmallContent">
           <p>10 Products</p>
-          <h1>Ice Cream</h1>
+          <h1>Milks</h1>
           <p>{props.botCap}</p>
         </div>
       </div>
 
       <div className="cardSmallBody">
-        <img src={Image1} alt="" />
+        <img src={Icecream} alt="" />
         <div className="cardSmallContent">
           <p>10 Products</p>
-          <h1>Ice Cream</h1>
+          <h1>Ice Creams</h1>
           <p>{props.botCap}</p>
         </div>
       </div>
 
       <div className="cardSmallBody">
-        <img src={Image1} alt="" />
+        <img src={Meat} alt="" />
         <div className="cardSmallContent">
           <p>10 Products</p>
-          <h1>Ice Cream</h1>
+          <h1>Meats</h1>
           <p>{props.botCap}</p>
         </div>
       </div>
 
       <div className="cardSmallBody">
-        <img src={Image1} alt="" />
+        <img src={Veggies} alt="" />
         <div className="cardSmallContent">
           <p>10 Products</p>
-          <h1>Ice Cream</h1>
+          <h1>Veggies</h1>
           <p>{props.botCap}</p>
         </div>
       </div>
