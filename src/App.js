a12d00d04769c4fdd99f7676074067333a39076b
Skinny_Vin <simonvicente38@gmail.com>
2023-03-24 01:15:18 +0800

    login page adedd

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import Navigationbar from "./Components/NavigationBar";
-import { BrowserRouter, Routes, Route, Navigate } from  "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./Pages/Home";
-import AboutUs from './Pages/AboutUs';
-import Dashboard from './Pages/Dashboard';
-import Orders from './Pages/Orders';
-import Shop from './Pages/Shop';
+import AboutUs from "./Pages/AboutUs";
+import Dashboard from "./Pages/Dashboard";
+import Orders from "./Pages/Orders";
+import Shop from "./Pages/Shop";
+import LoginPage from "./Pages/LoginPage";
 
 function App() {
   return (
-   <BrowserRouter>
-    <div className="App">
-      <Navigationbar />
-      <Routes>
-          <Route path = '/' element={<Home />}></Route>
-          <Route path = '/AboutUs' element={<AboutUs />}></Route>
-          <Route path = '/Dashboard' element={<Dashboard />}></Route>
-          <Route path = '/Orders' element={<Orders />}></Route>
-          <Route path = '/Shop' element={<Shop />}></Route>
-      </Routes>
-    
-    </div>
-   </BrowserRouter>
+    <BrowserRouter>
+      <div className="App">
+        <Navigationbar />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/AboutUs" element={<AboutUs />}></Route>
+          <Route path="/Dashboard" element={<Dashboard />}></Route>
+          <Route path="/Orders" element={<Orders />}></Route>
+          <Route path="/Shop" element={<Shop />}></Route>
+          <Route path="/Login" element={<LoginPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/Components/ShopSlider.js
+++ src/Components/ShopSlider.js
@@ -1,19 +1,13 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import image1 from "../Images/shop-images/slider1.jpg";
 import image2 from "../Images/shop-images/slider2.jpg";
 import image3 from "../Images/shop-images/slider3.jpg";
 import image4 from "../Images/shop-images/slider4.jpg";
 import image5 from "../Images/shop-images/slider5.jpg";
 import "../Components/ShopSlide.css";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
-const images = [
-    image1,
-    image2,
-    image3,
-    image4,
-    image5
-];
+const images = [image1, image2, image3, image4, image5];
 
 const Slider = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
@@ -26,30 +20,31 @@ const Slider = () => {
   }, [currentImageIndex]);
 
   const goToPreviousImage = () => {
-    const prevIndex = currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
+    const prevIndex =
+      currentImageIndex === 0 ? images.length - 1 : currentImageIndex - 1;
     setCurrentImageIndex(prevIndex);
     setBackground(
-      ['', '', '', '', ''].map((_, i) => (i === prevIndex ? 'green' : ''))
+      ["", "", "", "", ""].map((_, i) => (i === prevIndex ? "green" : ""))
     );
   };
-  
+
   const goToNextImage = () => {
     const nextIndex = (currentImageIndex + 1) % images.length;
     setCurrentImageIndex(nextIndex);
     setBackground(
-      ['', '', '', '', ''].map((_, i) => (i === nextIndex ? 'green' : ''))
+      ["", "", "", "", ""].map((_, i) => (i === nextIndex ? "green" : ""))
     );
   };
-  
+
   // For Pagination //
 
-  const [pagibackground, setBackground] = useState(['green', '', '', '', '']);
+  const [pagibackground, setBackground] = useState(["green", "", "", "", ""]);
   useEffect(() => {
     const timer = setInterval(() => {
       const nextIndex = (currentImageIndex + 1) % 5; // cycle through the 5 images
       setCurrentImageIndex(nextIndex);
       setBackground(
-        ['', '', '', '', ''].map((_, i) => (i === nextIndex ? 'green' : ''))
+        ["", "", "", "", ""].map((_, i) => (i === nextIndex ? "green" : ""))
       );
     }, 5000); // change every 3 seconds
 
@@ -58,63 +53,90 @@ const Slider = () => {
 
   const theActive = (event) => {
     const btnId = event.target.id;
-    switch (btnId){
-      case 'firstImg':
+    switch (btnId) {
+      case "firstImg":
         setCurrentImageIndex(0);
-        if (currentImageIndex >= 0){
-          setBackground(['green','','','','']);
+        if (currentImageIndex >= 0) {
+          setBackground(["green", "", "", "", ""]);
         }
         break;
-      case 'secondImg':
+      case "secondImg":
         setCurrentImageIndex(1);
-        if (currentImageIndex >= 0){
-          setBackground(['','green','','','']);
+        if (currentImageIndex >= 0) {
+          setBackground(["", "green", "", "", ""]);
         }
         break;
-      case 'thirdImg':
+      case "thirdImg":
         setCurrentImageIndex(2);
-        if (currentImageIndex >= 0){
-          setBackground(['','','green','','']);
+        if (currentImageIndex >= 0) {
+          setBackground(["", "", "green", "", ""]);
         }
         break;
-      case 'fourthImg':
+      case "fourthImg":
         setCurrentImageIndex(3);
-        if (currentImageIndex >= 0){
-          setBackground(['','','','green','']);
+        if (currentImageIndex >= 0) {
+          setBackground(["", "", "", "green", ""]);
         }
         break;
-      case 'fifthImg':
+      case "fifthImg":
         setCurrentImageIndex(4);
-        if (currentImageIndex >= 0){
-          setBackground(['','','','','green']);
+        if (currentImageIndex >= 0) {
+          setBackground(["", "", "", "", "green"]);
         }
         break;
       default:
         break;
     }
-  }
+  };
 
   return (
     <div className="slider">
-        <div className='shopImages'>
-            <img src={images[currentImageIndex]} alt={`Image ${currentImageIndex}`} />
+      <div className="shopImages">
+        <img
+          src={images[currentImageIndex]}
+          alt={`Image ${currentImageIndex}`}
+        />
 
-            <div className="buttons">
-                <div className="btnContainer1">
-                    <button onClick={goToPreviousImage} className="leftbtn"><i class="fa-solid fa-chevron-left"></i></button>
-                </div>
-                <div className="btnContainer2">
-                    <button onClick={goToNextImage} className="rightbtn"><i class="fa-solid fa-chevron-right"></i></button>
-                </div>
-            </div>
-          <div className='pagination'>
-            <button id='firstImg' onClick={theActive} style={{background: pagibackground[0]}}></button>
-            <button id='secondImg' onClick={theActive} style={{background: pagibackground[1]}}></button>
-            <button id='thirdImg' onClick={theActive} style={{background: pagibackground[2]}}></button>
-            <button id='fourthImg' onClick={theActive} style={{background: pagibackground[3]}}></button>
-            <button id='fifthImg' onClick={theActive} style={{background: pagibackground[4]}}></button>
+        <div className="buttons">
+          <div className="btnContainer1">
+            <button onClick={goToPreviousImage} className="leftbtn">
+              <i class="fa-solid fa-chevron-left"></i>
+            </button>
+          </div>
+          <div className="btnContainer2">
+            <button onClick={goToNextImage} className="rightbtn">
+              <i class="fa-solid fa-chevron-right"></i>
+            </button>
           </div>
         </div>
+        <div className="pagination">
+          <button
+            id="firstImg"
+            onClick={theActive}
+            style={{ background: pagibackground[0] }}
+          ></button>
+          <button
+            id="secondImg"
+            onClick={theActive}
+            style={{ background: pagibackground[1] }}
+          ></button>
+          <button
+            id="thirdImg"
+            onClick={theActive}
+            style={{ background: pagibackground[2] }}
+          ></button>
+          <button
+            id="fourthImg"
+            onClick={theActive}
+            style={{ background: pagibackground[3] }}
+          ></button>
+          <button
+            id="fifthImg"
+            onClick={theActive}
+            style={{ background: pagibackground[4] }}
+          ></button>
+        </div>
+      </div>
     </div>
   );
 };
