bdfe7c5a941d599135837a5d404f4475a3807133
Skinny_Vin <simonvicente38@gmail.com>
2023-03-26 19:54:52 +0800

    homepage addings

--- src/Components/ThirdSectionHomepage.js
+++ src/Components/ThirdSectionHomepage.js
@@ -1,10 +1,28 @@
 import "../Components/ThirdSection.css";
 import HomepageBanner from "./HomepageBanner";
+import ProdCat from "./ProductCategory";
+import Prods from "./Prods";
 
 const ThirdSectionHomepage = () => {
   return (
     <div className="thirdBody">
       <HomepageBanner />
+      <div className="prodFilter">
+        <div className="prods">
+          <Prods />
+        </div>
+        <div className="filter">
+          <ProdCat
+            heading="Product Categories"
+            category1="Beverages"
+            category2="Chips & Snaks"
+            category3="Health & Beauty"
+            category4="Meats"
+            category5="Milks & Daries"
+            category6="Vegetables"
+          />
+        </div>
+      </div>
     </div>
   );
 };
