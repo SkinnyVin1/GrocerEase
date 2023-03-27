c50783c0fff39c2af87b778f7378cf760bb96cb4
Skinny_Vin <118040316+SkinnyVin1@users.noreply.github.com>
2023-03-27 16:09:21 +0800

    login page revisions

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -1,9 +1,17 @@
 import "../Pages/LoginPage.css";
 import Bg from "../Images/loginbg.png";
-import ShoImg from "../Images/5865.png";
+import ShoImg from "../Images/shopping.png";
 import { Link } from "react-router-dom";
+import {useState} from 'react'
 
 const LoginPage = () => {
+  const [showPassword, setShowPassword] = useState(false);
+
+  const togglePasswordVisibility = (event) => {
+    event.preventDefault(); // Prevents the default form submission behavior
+    setShowPassword(!showPassword);
+  };
+  
   return (
     <div className="loginBody">
       <img src={Bg} id="bg" />
@@ -13,13 +21,39 @@ const LoginPage = () => {
             <img src={ShoImg} alt="" />
           </div>
           <div className="lForm">
-            <span>Welcome to</span>
-            <h1>GrocerEase</h1>
-            <ul>
-              <li>
-                <Link to="/">Go Back to Main Page</Link>
-              </li>
-            </ul>
+            <div className="welcome">
+              <span>Welcome to</span>
+              <h1>GrocerEase</h1>
+              <ul>
+                <li>
+                  <Link to="/">Go Back to Main Page</Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="logform">
+              <form action="">
+              <input type="text" placeholder='Username'className='inText'/>
+              <div className="passy">
+              <input type={showPassword ? "text" : "password"}
+              id="myInput" placeholder='Password'/>
+
+              <div className='showForgot'>
+              <Link to='/' id='forgot'>forgot password</Link>
+
+              <button onClick={togglePasswordVisibility} id='showHide'>
+               {showPassword ? "Hide" : "Show"} password
+               </button>
+               
+              </div>
+              </div>
+              
+              
+              <button>Log In</button>
+              
+              <Link to='/'> or create an account</Link>
+              </form>
+            </div>
           </div>
         </div>
       </div>
