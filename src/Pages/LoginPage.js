a8871fb630af13ce5442d17ea59788c3bcbced9f
Skinny_Vin <simonvicente38@gmail.com>
2023-03-28 00:10:22 +0800

    responsiveness updates

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -2,7 +2,7 @@ import "../Pages/LoginPage.css";
 import Bg from "../Images/loginbg.png";
 import ShoImg from "../Images/shopping.png";
 import { Link } from "react-router-dom";
-import {useState} from 'react'
+import { useState } from "react";
 
 const LoginPage = () => {
   const [showPassword, setShowPassword] = useState(false);
@@ -11,7 +11,7 @@ const LoginPage = () => {
     event.preventDefault(); // Prevents the default form submission behavior
     setShowPassword(!showPassword);
   };
-  
+
   return (
     <div className="loginBody">
       <img src={Bg} id="bg" />
@@ -30,28 +30,31 @@ const LoginPage = () => {
                 </li>
               </ul>
             </div>
-
+            <img src={ShoImg} id="hideImg" />
             <div className="logform">
               <form action="">
-              <input type="text" placeholder='Username'className='inText'/>
-              <div className="passy">
-              <input type={showPassword ? "text" : "password"}
-              id="myInput" placeholder='Password'/>
+                <input type="text" placeholder="Username" className="inText" />
+                <div className="passy">
+                  <input
+                    type={showPassword ? "text" : "password"}
+                    id="myInput"
+                    placeholder="Password"
+                  />
 
-              <div className='showForgot'>
-              <Link to='/' id='forgot'>forgot password</Link>
+                  <div className="showForgot">
+                    <Link to="/" id="forgot">
+                      forgot password
+                    </Link>
 
-              <button onClick={togglePasswordVisibility} id='showHide'>
-               {showPassword ? "Hide" : "Show"} password
-               </button>
-               
-              </div>
-              </div>
-              
-              
-              <button>Log In</button>
-              
-              <Link to='/'> or create an account</Link>
+                    <button onClick={togglePasswordVisibility} id="showHide">
+                      {showPassword ? "Hide" : "Show"} password
+                    </button>
+                  </div>
+                </div>
+
+                <button>Log In</button>
+
+                <Link to="/"> or create an account</Link>
               </form>
             </div>
           </div>
